a482607e43316fbe7dcdd0e9724b47ce6d91edd0
Mateusz Domurad <vimparek@gmail.com>
2018-01-10 10:50:19 +0100

    0.0.6.1
    Edited Contact Title
    Edited Index Controller View Render

--- backend/controllers/index.class.php
+++ backend/controllers/index.class.php
@@ -4,7 +4,7 @@
 
     function __construct($params) {
       parent::__construct();
-      $this -> view -> controller = 'index';
+      $this -> view -> controller = 'control';
       $this -> view -> render();
     }
 
